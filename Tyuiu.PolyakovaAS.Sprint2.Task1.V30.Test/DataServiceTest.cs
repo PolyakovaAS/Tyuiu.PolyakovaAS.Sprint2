ccbdf2def6e5b97bd4ed1c40fd03087a7f22611c
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-14 23:59:39 +0300

    Task1

---
+++ Tyuiu.PolyakovaAS.Sprint2.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PolyakovaAS.Sprint2.Task1.V30.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint2.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 657;
+            int b = 874;
+            int c = 14;
+            int d = 654;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, false, true, true, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
