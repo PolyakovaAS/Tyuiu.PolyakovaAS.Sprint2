093f58c768895996e189377c40f3512a08636850
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-14 23:52:36 +0300

    Add project files.

---
+++ Tyuiu.PolyakovaAS.Sprint2.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PolyakovaAS.Sprint2.Task0.V13.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint2.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1905;
+            int y = 475;
+            bool[] res = new bool[6]; 
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[] { false, true, false, true, false, true };
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
