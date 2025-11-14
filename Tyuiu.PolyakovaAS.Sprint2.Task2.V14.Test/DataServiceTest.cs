38c26393d60f652e04f36c83382a5fd1fd9eea53
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-15 00:25:12 +0300

    Task2

---
+++ Tyuiu.PolyakovaAS.Sprint2.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PolyakovaAS.Sprint2.Task2.V14.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint2.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 3, y = 3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
